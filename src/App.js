6b5181e3624431983285a89731abe0e2a85db0ca
馬場英次郎 <33510560+horsejiro@users.noreply.github.com>
2021-10-11 19:55:02 +0900

    fifth commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import { element } from "./view/html-util.js";
+import { TodoItemModel } from "./model/TodoItemModel.js";
+import { TodoListModel } from "./model/TodoListModel.js";
+import { element, render } from "./view/html-util.js";
 
 export class App {
+    constructor() {
+        this.TodoListModel = new TodoListModel();
+    }
     mount() {
         const formElement = document.querySelector("#js-form");
         const inputElement = document.querySelector("#js-form-input");
         const containerElement = document.querySelector("#js-todo-list");
         const todoItemCountElement = document.querySelector("#js-todo-count");
 
-        let todoItemCount = 0;
+        this.TodoListModel.onChange(() => {
+            const todoListElement = element`<ul />`;
+            const todoItems = this.TodoListModel.getTodoItems();
+
+            todoItems.forEach(item => {
+                const todoItemElement = element`<li>${item.title}</li>`;
+                todoListElement.appendChild(todoItemElement);
+            });
+
+            render(todoListElement, containerElement);
+            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+        })
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
 
-            const todoItemElement = element`<li>${inputElement.value}</li>`;
-            containerElement.appendChild(todoItemElement);
-            
-            todoItemCount += 1;
-            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
+            this.TodoListModel.addTodo(new TodoItemModel({
+                title: inputElement.ariaValueMax,
+                completed: false
+            }));
             inputElement.value = "";
         });
     }
